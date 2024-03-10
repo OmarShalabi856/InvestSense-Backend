63b99f0479de2f914fb6a95509016594494639e6
Omar Shalabi <omarshalabi856@gmail.com>
2024-03-10 20:23:19 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InvestSense_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestSense_API.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		
+		public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+		{
+
+
+		}
+
+		public DbSet<Stock> Stock { get; set; }
+		public DbSet<Comment> Comment { get; set; }
+
+
+	}
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace InvestSense_API.Models
+{
+	public class Comment
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public string Conent { get; set; } = string.Empty;
+
+		public DateTime CreatedOn { get; set; } = DateTime.Now;
+		public int? StockId { get; set; }
+		public Stock? Stock { get; set; }
+	}
+}
